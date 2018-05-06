d38635f136fa0013a166531d38bea61a2666a4a5
Max <max.vitvitckiy@gmail.com>
2018-05-06 20:51:03 +0300

    View

---
+++ name/name/View/mainView.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace name.View
+{
+    class mainView
+    {
+        public void StartGame()
+        {
+            Console.WriteLine("\tGAME FISH");
+            Level l = new Level();
+            l.StartLevel1();
+        }
+    }
+}
