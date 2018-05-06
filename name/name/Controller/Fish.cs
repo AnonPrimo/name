c6b20391dc5cb4acceb9f144f9bfbd0e69fe60fd
Julia <julie78sd@gmail.com>
2018-05-06 21:14:17 +0300

    seaclass

---
+++ name/name/Controller/Fish.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using name.Model;
+
+namespace name.Controller
+{
+    class Fish
+    {
+        List<SmallFish> fish = new List<SmallFish>();
+
+        public Fish()
+        {
+            fish.Add(new SmallFish());
+            fish.Add(new MediumFish());
+            fish.Add(new BigFish());           
+        }
+
+         public Fish(int n)
+        {
+
+        }
+
+        
+
+    }
+}
---
+++ name/name/Controller/SeaClass.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using name.Model;
+
+namespace name.Controller
+{
+    class SeaClass
+    {
+        SmallFish[,] sea = new SmallFish[,] {
+               { new SmallFish(),   new MediumFish(),   new BigFish() },
+               { null,              null,               null},
+               { null,              null,               null}
+            };
+
+        public SeaClass()
+        {
+            //random sea generation
+        }
+
+      
+        public SmallFish[,] Sea(int n)
+        {
+            int fishSize = n + 2, size = n * 3;
+          
+            //for (int i = 0; i < fishSize; i++)
+            //    for (int j = 0; j < size; j++)
+            //        sea[i, j] = ' ';        //fill "sea" with spaces
+
+           // Fish fish = new Controller.Fish();
+
+            //Random rand = new Random();
+            //for (int i = 0; i < fishSize; i++)
+            //{
+            //    int col = rand.Next(1, size);
+            //    int row = rand.Next(1, size);
+
+            //    if (sea[row, col] == ' ')
+            //        sea[row, col] = ;      //random fill "fish"
+            //    else
+            //        i--;
+            //}
+
+            //for (int i = fishSize - 1; i > 0; i--)
+            //{
+            //    for (int j = 0; j < size; j++)
+            //    {
+            //        if (sea[i, j] != ' ')
+            //        {
+            //            while (true)
+            //            {
+            //                if (sea[i - 1, j] != ' ')
+            //                    break;
+            //                sea[i - 1, j] = sea[i, j];  //підйом риби якомога вище
+            //                sea[i, j] = ' ';
+            //            }
+            //        }
+            //    }
+            //}
+
+            return sea;
+        }
+    }
+}
