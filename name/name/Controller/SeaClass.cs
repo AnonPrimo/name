2c2536017cba94a6965624e45f37c0529ba64f55
Олександр <viryz1998@gmail.com>
2018-05-12 19:50:55 +0300

    Змінні для кількості рядків та стовбців масиву. Конструктор з параметрами(кількість рядків і совбців) В методі StartNewLevel перевірка співпадання розмірності.

--- name/name/Controller/SeaClass.cs
+++ name/name/Controller/SeaClass.cs
@@ -19,24 +19,49 @@ namespace name.Controller
         SmallFish currentFish;
         SmallFish findFish;
 
+        /// <summary>
         /// Лічильник якусів для рандомної генерації.
+        /// </summary>
+        private int counter;
 
-        private int Counter; 
+        /// <summary>
+        /// Кількість рядків в масиві.
+        /// </summary>
+        private int rows;
+
+        /// <summary>
+        /// Кількість стовбців в масиві.
+        /// </summary>
+        private int cols;
 
         /// <summary>
         /// Створення нового рівня
         /// </summary>
         /// <param name="Counter">Лічильник ярусів для рандомної генерації</param>
         /// <param name="sea">Масив з рибками</param>
-        public void StartNewLevel(int Counter, SmallFish [,] sea)
+        public void StartNewLevel(int counter, SmallFish [,] sea)
         {
-            this.Counter = Counter;
+            if (sea.Length != this.sea.Length)
+                throw new Exception("Розмірності масивів не співпадають");
+
+            this.counter = counter;
             this.sea = sea;
         }
 
-        public SeaClass()
+        /// <summary>
+        /// Конструктор SeaClass
+        /// </summary>
+        /// <param name="cols">Кількість стовбців</param>
+        /// <param name="rows">Кількість рядків</param>
+        public SeaClass(int cols, int rows)
         {
-            ///random sea generation
+            this.cols = cols;
+            this.rows = rows;
+            sea = new SmallFish[rows, cols]; // ініціалізація розмірнсті.
+
+            for (int i = 0; i < rows; i++) // Заповнення null.
+                for (int j = 0; j < cols; j++)
+                    sea[i, j] = null;
         }
 
         public SmallFish find(int a)
