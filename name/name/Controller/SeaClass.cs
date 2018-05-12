79c218cac9850eb346b5d1a732983bba9e2a8fa7
Олександр <viryz1998@gmail.com>
2018-05-12 20:17:16 +0300

    Видалив коментарі

--- name/name/Controller/SeaClass.cs
+++ name/name/Controller/SeaClass.cs
@@ -19,27 +19,16 @@ namespace name.Controller
         SmallFish currentFish;
         SmallFish findFish;
 
-        /// <summary>
-        /// Лічильник якусів для рандомної генерації.
-        /// </summary>
+       
         private int Counter;
 
-        /// <summary>
-        /// Кількість рядків в масиві.
-        /// </summary>
+        
         private int rows;
 
-        /// <summary>
-        /// Кількість стовбців в масиві.
-        /// </summary>
+        
         private int cols;
 
-        /// <summary>
-        /// Створення нового рівня
-        /// </summary>
-        /// <param name="Counter">Лічильник ярусів для рандомної генерації</param>
-        /// <param name="sea">Масив з рибками</param>
-        /// 
+        
         public void StartNewLevel(int Counter, SmallFish [,] sea)
         {
             if (sea.Length != this.sea.Length)
@@ -49,11 +38,7 @@ namespace name.Controller
             this.sea = sea;
         }
 
-        /// <summary>
-        /// Конструктор SeaClass
-        /// </summary>
-        /// <param name="cols">Кількість стовбців</param>
-        /// <param name="rows">Кількість рядків</param>
+        
         public SeaClass(int cols, int rows)
         {
             this.cols = cols;
